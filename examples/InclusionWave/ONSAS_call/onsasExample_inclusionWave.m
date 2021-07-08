c70ad4e830faceb121899e0e0faec8629b308faa
jorgepz <jorgepz@fing.edu.uy>
2021-07-07 22:24:08 -0300

    cambios en parámetros y agrego archivo mat

--- examples/InclusionWave/ONSAS_call/onsasExample_inclusionWave.m
+++ examples/InclusionWave/ONSAS_call/onsasExample_inclusionWave.m
@@ -18,7 +18,7 @@ thickness = 1 ;
 density = 950 ; % parametro promedio del tejido mamario
 
 materials.hyperElasModel  = {'linearElastic'; 'linearElastic'} ;
-materials.hyperElasParams = { [ E nu ]; [ 5*E nu ] }      ;
+materials.hyperElasParams = { [ E nu ]; [ 10*E nu ] }      ;
 materials.density         = { density ; density }      ;
 
 elements.elemType = { 'node', 'edge', 'triangle' } ;
@@ -27,7 +27,7 @@ elements.elemTypeParams = { []; [] ; 2  } ;
 elements.elemTypeGeometry = { []; thickness ; thickness } ;
 
 boundaryConds.loadsCoordSys = {[]; []; 'global'  } ;
-boundaryConds.loadsTimeFact = { []; []; @(t) (t<1e-3)  } ;
+boundaryConds.loadsTimeFact = { []; []; @(t) 1e4*( 3*(t<5e-5) - 3*(t<10e-5) + 1*(t<15e-5) ) } ;
 boundaryConds.loadsBaseVals = { []; []; [ 0 0 -p 0  0 0 ]  } ;
 boundaryConds.imposDispDofs = { [1 3] ; [3] ; []  } ;
 boundaryConds.imposDispVals = { [0 0] ; [0] ; []  } ;
@@ -42,15 +42,14 @@ analysisSettings.methodName    = 'newmark' ;
 analysisSettings.stopTolIts    = 30      ;
 analysisSettings.stopTolDeltau = 1.0e-12 ;
 analysisSettings.stopTolForces = 1.0e-12 ;
-analysisSettings.finalTime      = 1e-2    ;
+analysisSettings.finalTime      = 150e-5    ;
 analysisSettings.alphaNM       = 0.25    ;
-analysisSettings.deltaT        = 1e-4    ;
+analysisSettings.deltaT        = .25e-5    ;
 analysisSettings.deltaNM       = 0.5     ;
 %md
 %md
 %md### Output parameters
 otherParams.problemName = 'inclusionWave' ;
 otherParams.plotsFormat = 'vtk' ;
-otherParams.spitMatrices = true ;
 %md
 [matUs, loadFactorsMat] = ONSAS( materials, elements, boundaryConds, initialConds, mesh, analysisSettings, otherParams ) ;
